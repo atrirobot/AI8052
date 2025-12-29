867fbf87cd7f303c65f8c57cfb6eab8da13b37a0
atrirobot <155962843+atrirobot@users.noreply.github.com>
2025-12-29 17:02:22 +0800

    Add files via upload

--- Seekfree_STC32G144K_100Pin_Opensource_Library/project/user/main.c
+++ Seekfree_STC32G144K_100Pin_Opensource_Library/project/user/main.c
@@ -1,52 +1,63 @@
-/*********************************************************************************************************************
-* STC32G144K Opensourec Library 即（STC32G144K 开源库）是一个基于官方 SDK 接口的第三方开源库
-* Copyright (c) 2025 SEEKFREE 逐飞科技
-*
-* 本文件是STC32G144K开源库的一部分
-*
-* STC32G144K 开源库 是免费软件
-* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
-* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
-*
-* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
-* 甚至没有隐含的适销性或适合特定用途的保证
-* 更多细节请参见 GPL
-*
-* 您应该在收到本开源库的同时收到一份 GPL 的副本
-* 如果没有，请参阅<https://www.gnu.org/licenses/>
-*
-* 额外注明：
-* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
-* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
-* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
-* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
-*
-* 文件名称          
-* 公司名称          成都逐飞科技有限公司
-* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
-* 开发环境          MDK FOR C251
-* 适用平台          STC32G144K
-* 店铺链接          https://seekfree.taobao.com/
-*
-* 修改记录
-* 日期              作者           备注
-* 2025-11-20        大W            first version
-********************************************************************************************************************/
 #include "zf_common_headfile.h"
 
+uint8 rx_buffer[128]; 
+uint32 rx_len = 0;
+float total_distance=10.0f;
+void PID_Calc(PID_Type *pid) {
+    pid->err = pid->target_val - pid->actual_val;
+    pid->integral += pid->err;
+    pid->output = (pid->Kp * pid->err) + (pid->Ki * pid->integral) + (pid->Kd * (pid->err - pid->last_err));
+    pid->last_err = pid->err;
+    pid->actual_val += pid->output * 0.05f; 
+}
+float a=0;
 void main(void)
 {
-    clock_init(SYSTEM_CLOCK_96M); 				// 时钟配置及系统初始化<务必保留>
-    debug_init();                       		// 调试串口信息初始化
-
-    // 此处编写用户代码 例如外设初始化代码等
-
-    // 此处编写用户代码 例如外设初始化代码等
-
+    uint32 loop_count = 0;
+    clock_init(SYSTEM_CLOCK_108M); 				
+    debug_init();
+//	pit_ms_init(TIMER0_IRQn,5);	
+//	interrupt_set_priority(TIMER0_IRQn,0);
+	gpio_init(IO_P52, GPO, 1, GPO_PUSH_PULL);
+	gpio_set_level(IO_P52, 0);
+    printf("Wait Connect...\n");
+    while(wifi_spi_init("Ciallo", "0d000721") != 0) {
+        system_delay_ms(1000); 
+    }
+    printf("Success!\n");
     while(1)
     {
-        // 此处编写需要循环执行的代码
-
-        // 此处编写需要循环执行的代码
+        // 1. 接收数据
+        rx_len = wifi_spi_read_buffer(rx_buffer, 127);
+        if(rx_len > 0)
+        {
+            rx_buffer[rx_len] = 0; 
+            printf("RX: %s\n", rx_buffer);
+            if(my_strstr((char*)rx_buffer, "SET_PID")) {
+                Parse_PID_Command((char*)rx_buffer);
+            }
+            else if(my_strstr((char*)rx_buffer, "CMD_LED")) {
+                gpio_set_level(IO_P52, !gpio_get_level(IO_P52));
+                Send_Text("LED Toggled\n");
+            }
+        }
+        // 2. PID 仿真
+        PID_Calc(&steer_loop);
+        // 3. 定时发送状态 (每 200ms)
+        loop_count++;
+        if(loop_count % 20 == 0) {
+			char log_msg[64];
+			a++;
+			total_distance++;
+			speed_loop.actual_val+=0.1f;
+            loop_count = 0;  
+            //sprintf(log_msg, "Steer P:%.2f I:%.2f D:%.2f\n", steer_loop.Kp, steer_loop.Ki, steer_loop.Kd);
+            //wifi_spi_send_string(log_msg);
+			sprintf(log_msg, "DAT:%.1f:%d:%.1f:Running\n", speed_loop.actual_val,(int)total_distance,a);
+			Send_Text(log_msg);
+        }
+		Send_Image((uint8*)image_data,0); // ★ 发送图片
+		if(loop_count > 1000) loop_count = 0;
+        system_delay_ms(100); 
     }
 }
